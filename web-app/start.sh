3bd515cf1af6ae679591e541cf41b0e21b4cf917
YourTechBud <noorain@spaceuptech.com>
2019-07-16 18:11:57 +0530

    added web app runtime

--- web-app/start.sh
+++ web-app/start.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
-
+mkdir public
+mkdir -p "public$PREFIX"
 echo "Downloading the package ..."
 wget --header "Authorization: Bearer $REGISTRY_TOKEN" -O file.zip "$REGISTRY_URL/v1/registry/deployment/$PROJECT/$NAME.zip"
 echo "Done!"
 echo "Extracting archive ..."
-unzip -u file.zip
+unzip -u file.zip -d "public$PREFIX"
 rm file.zip
 echo "Done!"
-echo "Running the install command: $CMD_INSTALL"
-eval $CMD_INSTALL
-echo "Done!"
-echo "Running the execute command: $CMD_RUN"
-eval $CMD_RUN
+echo "Hosting the static content"
+serve -l 8000 -s public

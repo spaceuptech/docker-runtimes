7e30346ad9d735b8cf9e3bfd072a90b763eaedec
Aliabbas Merchant <merchantaliabbas@gmail.com>
2019-07-21 13:23:11 +0530

    Added Images for Go, Java, Python

---
+++ go-1.12/start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Downloading the package ..."
+wget --header "Authorization: Bearer $REGISTRY_TOKEN" -O file.zip "$REGISTRY_URL/v1/registry/deployment/$PROJECT/$NAME.zip"
+echo "Done!"
+echo "Extracting archive ..."
+unzip -u file.zip
+rm file.zip
+echo "Done!"
+
+# We will get prebuilt executables, so no need to install anything
+# echo "Running the install command: $CMD_INSTALL"
+# eval $CMD_INSTALL
+# echo "Done!"
+
+echo "Running the execute command: $CMD_RUN"
+eval $CMD_RUN
